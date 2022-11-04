2e415248e419cd73015d94132d522abc9f98f480
Possidonio Neto <102385082+PossidonioNeto@users.noreply.github.com>
2022-11-04 17:09:23 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'PT',
+                vice: 'Alckimin',
+                fotos:[
+                    {url:'13.jpg', legenda: 'Presidente'},
+                    {url:'13_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'PL',
+                vice: 'Mourão',
+                fotos:[
+                    {url:'22.jpg', legenda: 'Presidente'},
+                    {url:'22_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'Ciro',
+                partido: 'PDT',
+                vice: 'Ana Paula',
+                fotos:[
+                    {url:'12.jpg', legenda: 'Presidente'},
+                    {url:'12_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Simone Tabet',
+                partido: 'MDB',
+                vice: 'Mara Gribilli',
+                fotos:[
+                    {url:'15.jpg', legenda: 'Presidente'},
+                    {url:'15_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
